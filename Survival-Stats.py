4923d45bdba405f6e076053cee48a78563d960df
mtuong79 <89337936+mtuong79@users.noreply.github.com>
2021-09-09 22:47:20 +0700

    Update Survival-Stats.py

--- Survival-Stats.py
+++ Survival-Stats.py
@@ -1,5 +1,6 @@
 # https://analyticsindiamag.com/building-a-covid-19-dashboard-using-streamlit/
 # https://realpython.com/pandas-groupby/
+# https://scholar.uwindsor.ca/cgi/viewcontent.cgi?article=8997&context=etd
 
 import streamlit as st
 import pandas as pd
@@ -8,7 +9,7 @@ from datetime import date
 
 def Stats(df,name,age,sex):
 
-    st.write("Record your SpO2 and Heart-rate")
+    st.write("Record your Hear-Rate and Sp02")
     file = 'Data/survival-stats.csv'
     df['Age'] = df['Age'].astype(int)
     df['BP'] = df['BP'].astype(int)
@@ -19,8 +20,8 @@ def Stats(df,name,age,sex):
         today = date.today()
         order_date = st.date_input('Today', today)
         sl_time = st.radio("",("Morning","Afternoon"))
-        SP02_rate = st.number_input("SpO2", value=0)
-        BP_rate = st.number_input("Heart-rate", value=0)
+        SP02_rate = st.number_input("Sp02", value=0)
+        BP_rate = st.number_input("BP", value=0)
         submitted = st.form_submit_button("Submit")
 
         if submitted:
@@ -49,7 +50,7 @@ def Stats(df,name,age,sex):
 
 def Report(df):
     st.write("Report")
-    person = st.radio("",('Tuong','Thu','Danh'))
+    person = st.radio("",('Tuong','Thu','Danh','Heart Rate'))
 
     if person == 'Tuong':
         st.subheader("Survival Stats of Tuong")
@@ -67,21 +68,41 @@ def Report(df):
         st.area_chart(thu_chart[['BP','SpO2']])
         st.write(thu_chart)
 
-    else:
+    elif person == 'Danh':
         st.subheader("Survival Stats of Danh")
         df_chart = df.groupby('Name')
         danh_chart = df_chart.get_group("Danh")
         st.line_chart(danh_chart[['BP','SpO2']])
         st.area_chart(danh_chart[['BP','SpO2']])
         st.write(danh_chart)
-
+    else:
+        st.subheader("Approximate maximum heart rate")
+        age = st.number_input("Your Age", value=0)
+        max_HR = 220 - age
+        st.write(f"The maximum heart rate of you is **{max_HR}** beats per minute")
+        st.subheader("Standard of Heart Rate")
+        st.image('img/BP_Standard.png')
+        st.subheader("What is a normal heart rate?")
+        st.markdown("A normal heart rate, when you're not being active, is between 60 – 100 beats per minute. "
+                    " is called your resting heart rate. If you've been active, you'll need to wait at least five minutes before taking your pulse.")
+        st.markdown("When you're active, your heart beats faster to get more oxygen to your working muscles. The harder your body is working, the faster your heart will beat. "
+                    "For example, your heart rate when you're sprinting will be much faster than your heart rate when you're walking. "
+                    "If you're exercising hard it's normal for your heart rate to get up to 160 beats per minute or more.")
+        st.markdown("There are other things that can make your heart beat faster, like caffeine, nicotine, recreational drugs and some kinds of medications. "
+                    "Your heart will also beat faster when you feel strong emotions, like anxiety or fear.")
+        st.markdown("Athletes or people who are very fit may have resting heart beats of less than 60 bpm.")
+        st.subheader("Exercise and heart rate")
+        st.markdown("Like any other muscle, your heart needs exercise to keep it fit and healthy. "
+                    "Regular exercise can help reduce your risk of heart disease and other health conditions, such as diabetes.")
+        st.markdown("To keep your heart healthy, you should aim to do 150 minutes of low to moderate intensity exercise a week.")
 
 def main():
-    st.title("SURVIVAL STATS")
+    st.title("VITAL SIGN")
 
     st.image("img/jumper.jpg")
 
-    file = "Data/survival-stats.csv"
+    file = "Data/Survival-Stats.csv"
+    # df = get_df(file)
     df = pd.read_csv(file)
                      #index_col=['Date'])
 
